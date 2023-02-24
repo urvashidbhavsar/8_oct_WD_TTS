2c517ec64a4c23d9129702a81988119fc5a5fd45
Urvashi D. Bhavsar <109721868+urvashidbhavsar@users.noreply.github.com>
2023-02-24 09:02:35 +0530

    Add files via upload

---
+++ bootstrap/08_23-02-2023/webpage/owl-carousel.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:false,
+        dots:true,
+        autoplay:true,
+        autoplayTimeout:2000,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:3
+            }
+        }
+    })
+})
